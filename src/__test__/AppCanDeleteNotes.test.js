6e0daae9daa6dd5a9fd7799931f87c15114378d9
Henning Muszynski <henningmu@users.noreply.github.com>
2019-01-22 13:01:17 +0100

    Cleaned app for workshop

--- src/__test__/AppCanDeleteNotes.test.js
+++ src/__test__/AppCanDeleteNotes.test.js
@@ -4,18 +4,5 @@ import { render, fireEvent } from 'react-testing-library'
 import App from '../App'
 
 it('can delete a note', () => {
-    const { getByTestId, queryByTestId } = render(<App />)
-    expect(getByTestId('composer-input')).toBeDefined()
-    expect(getByTestId('submit-button')).toBeDefined()
-    expect(queryByTestId('note-content')).toBeNull()
-
-    fireEvent.change(getByTestId('composer-input'), { target: { value: 'Test Text' } })
-    expect(getByTestId('composer-input').value).toBe('Test Text')
-
-    fireEvent.click(getByTestId('submit-button'))
-    expect(getByTestId('note-content').textContent).toBe('Test Text')
-    expect(getByTestId('delete-note')).toBeDefined()
-
-    fireEvent.click(getByTestId('delete-note'))
-    expect(queryByTestId('note-content')).toBeNull()
+    // Bonus: Implement me
 })
--- src/__test__/AppCanTogglePinnedNotes.test.js
+++ src/__test__/AppCanTogglePinnedNotes.test.js
@@ -4,25 +4,5 @@ import { render, fireEvent } from 'react-testing-library'
 import App from '../App'
 
 it('can toggle showing pinned notes', () => {
-    const { getByTestId, queryByTestId, container } = render(<App />)
-
-    expect(getByTestId('composer-input')).toBeDefined()
-    expect(getByTestId('submit-button')).toBeDefined()
-    expect(queryByTestId('note-content')).toBeNull()
-
-    fireEvent.change(getByTestId('composer-input'), { target: { value: 'Note 1' } })
-    fireEvent.click(getByTestId('submit-button'))
-    fireEvent.click(getByTestId('pin-note'))
-    fireEvent.change(getByTestId('composer-input'), { target: { value: 'Note 2' } })
-    fireEvent.click(getByTestId('submit-button'))
-
-    expect(container.querySelectorAll('.note').length).toBe(2)
-    expect(getByTestId('pinned-toggle')).toBeDefined()
-
-    fireEvent.click(getByTestId('pinned-toggle'))
-    expect(container.querySelectorAll('.note').length).toBe(1)
-    expect(getByTestId('note-content').textContent).toBe('Note 1')
-
-    fireEvent.click(getByTestId('pinned-toggle'))
-    expect(container.querySelectorAll('.note').length).toBe(2)
+    // Bonus: Implement me
 })
